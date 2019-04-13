3c490ea6926cb563e6303092cc3b1d1b2571ab11
Ismail <ismailelbadawyw@gmail.com>
2019-04-13 12:37:35 -0700

    Hash table can insert and search.

--- hash_table.c
+++ hash_table.c
@@ -20,12 +20,14 @@ typedef struct {
     char* type;
     int scope;
 
-} symbol;int getHash();
+} symbol;
 
-typedef struct{
+struct node {
     char* key;
     symbol* value;
-} node;
+
+    struct node * next;
+} *table[CHAIN];
 
 int getHash(char* variableName)
 {
@@ -35,8 +37,60 @@ int getHash(char* variableName)
     {
         sum += variableName[i];
     }
+    // get the modulu of the sum with the chain.
     return (sum % CHAIN);
 }
+
+bool insertSymbol(symbol * newSymbol)
+{
+    struct node * newNode;
+    newNode->key = newSymbol->variableName;
+    newNode->value = newSymbol;
+
+    int index = getHash(newNode->key);
+
+    // Check if the variable name exists in the table before.
+    // namely, the variable name is seen before with the same scope id.
+    if(searchSymbol(newSymbol))
+    {
+        return false;
+    }
+
+    // The symbol does not exist, meaning we can add it.
+    if(table[index] == NULL)
+    {
+        // The linked list is empty, so we need to initialize it.
+        table[index] = newNode;
+        newNode->next = NULL;
+    } else {
+
+        // Adding the node in the head of the list, this is just faster.
+        struct node* oldHead = table[index];
+        newNode->next = oldHead;
+        table[index] = newNode;
+    }
+    return true;
+}
+
+/// Searches for a symbol, returns true if it already exists.
+/// Returns false if it does not.
+bool searchSymbol(symbol * searchKey)
+{
+    int predictedIndex = getHash(searchKey->variableName);
+    struct node* predictedNode = table[predictedIndex];
+
+    while(predictedNode != NULL){
+        if(strcmp(predictedNode->key, searchKey->variableName) && predictedNode->value->scope == searchKey->scope)
+        {
+            // We found it.
+            return true;
+        }
+        predictedNode = predictedNode->next;
+    }
+    return false;
+}
+
+
 
 void main()
 {
