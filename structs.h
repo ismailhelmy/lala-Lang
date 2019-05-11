e1c43ef89c0f0638a6fd2a57ef06cdb060862397
ismailhelmy <38408394+ismailhelmy@users.noreply.github.com>
2019-05-11 13:04:56 +0200

    ok.

--- structs.h
+++ structs.h
@@ -4,9 +4,10 @@
 #include <stdio.h>
 
 typedef enum { typeCon, typeId, typeOpr } nodeEnum;
-
+typedef enum { typeint, typefloat, typestring, typebool, typevoid} conTypeEnum;
 /* constants */
 typedef struct {
+    conTypeEnum type;
     /* value of constants */
 	int flag;    /* 0-->int, 1-->float, 2-->char, 3-->string */
 	char* value;
@@ -18,6 +19,7 @@ typedef struct {
 	int flag;    /* 0->int, 1->float, 2->char, 3->string, 4->bool, 5->const int, 6->const float, 7->const char, 8->const string, 9->const bool*/
 	int permission; /* -1-> not declared, -2-> const, -3-> already defined */
 	char* name;
+    int scopeId;
 } idNodeType;
 
 /* operators */
